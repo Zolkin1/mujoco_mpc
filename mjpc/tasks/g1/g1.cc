ecba4795109e0137cfd32b1a6722425d7d241420
Zach Olkin <zach.olkin@gmail.com>
2025-04-18 11:44:06 -0700

    getting started on adding the g1 tasks

---
+++ mjpc/tasks/g1/g1.cc
@@ -0,0 +1,5 @@
+//
+// Created by zolkin on 4/18/25.
+//
+
+#include "g1.h"
---
+++ mjpc/tasks/g1/g1.h
@@ -0,0 +1,16 @@
+//
+// Created by zolkin on 4/18/25.
+//
+
+#ifndef G1_H
+#define G1_H
+
+
+
+class g1 {
+
+};
+
+
+
+#endif //G1_H
